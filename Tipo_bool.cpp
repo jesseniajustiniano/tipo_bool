3cddf01df9a32aa0a16ab5b480f7942dfde2d047
jesseniajustiniano <155786502+jesseniajustiniano@users.noreply.github.com>
2024-01-12 19:43:49 -0400

    Add files via upload

---
+++ Tipo_bool.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	//declaracion y uso de bool
+	bool b0;
+	b0=true;
+	cout<<"variable con valor TRUE:"<<b0<<endl;
+	b0=false;
+	cout<<"variable con valor FALSE:"<<b0<<endl;
+	//asignacion de valores numericos.
+	//si es cero es FALSO, si diferente de cero en VERDADERO.
+	b0=0; //el valor se vera con false
+	cout<<"variable con el valor 0:"<<b0<<endl;
+	b0=1; //el valor se vera como true.
+	cout<<"variable con valor 1:"<<b0<<endl;
+	b0=100; //el valor se vera como true.
+	cout<<"variable con valor -100:"<<b0<<endl;
+	return 0;
+}
+
