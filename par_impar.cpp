04fc963daee51e060d7b6f810f5f7fd362d9729d
jesseniajustiniano <155786502+jesseniajustiniano@users.noreply.github.com>
2024-01-12 21:56:43 -0400

    Add files via upload

---
+++ par_impar.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	//solicitar un numero y verificar y si par oimpar 
+	//mod en c++se utiliza %
+	int numero;
+	cout<<"ingrese numero";
+	cin>>numero;
+	if(numero%2==0);
+	 cout<<"el numero:"<<numero<<" es PAR"<<endl;
+	else
+		cout<<"el numer:"<<numero>>" es IMPAR"<<endl;
+	return 0;
+}
+
