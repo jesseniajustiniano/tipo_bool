f597cd91b262192e7aa769ad42704bfd5646974b
jesseniajustiniano <155786502+jesseniajustiniano@users.noreply.github.com>
2024-01-12 21:56:15 -0400

    Add files via upload

---
+++ resultado.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int resultado = 0;
+	cout << "cuenato es 10+5? ";
+	cin >> resultado;
+	if(resultado ==(10+5))
+	{
+		cout <<"la respuesta es correcta. FELICITACIONES!\n";
+	}
+	else
+	{
+		cout <<"la respuesta es incorrecta.!\n";
+	}
+	return 0;
+}
+
