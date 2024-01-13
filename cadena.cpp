9dcfaefe372de1403424e1802a70f4ae2cf4152e
jesseniajustiniano <155786502+jesseniajustiniano@users.noreply.github.com>
2024-01-12 21:57:42 -0400

    Add files via upload

---
+++ cadena.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	string cadena1, cadena2, universidad;
+	cadena1="Bienvenido a";
+	cadena2="univerdidad";
+	cout<<"ingrese el nombre de la univerdidad";
+	cin>>universidad;
+	
+	cout<<cadena1<<" la "<<cadena2<<" "<<universidad<<endl;
+	return 0;
+}
+
