65b2502ced28f4435724ea9e6c01cc02644f4b19
jesseniajustiniano <155786502+jesseniajustiniano@users.noreply.github.com>
2024-01-12 21:57:17 -0400

    Add files via upload

---
+++ constante.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+//#define PI 3.14176; //defimos una constante llamada PI  
+
+int main(int argc, char *argv[]) {
+	const float PI = 3.1416;//definimos una constaste llamada PI
+	double diametro, circunferencia;
+	circunferencia=40;
+	diametro=circunferencia/PI;
+	cout<<"El diametrode la circunferencia=40:"<<diametro; // <<endl;
+	//PI=0; //error por asignaci[on a una variable de solo lectura.
+	
+	return 0;
+}
+
